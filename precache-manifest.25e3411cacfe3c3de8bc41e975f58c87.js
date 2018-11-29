134505f1cc8a477462c72096cbea709fb09b4530
Fredrik Strand Oseberg <fredrik.strand.oseberg@avinor.no>
2018-11-29 18:38:07 +0100

    Updates

---
+++ precache-manifest.25e3411cacfe3c3de8bc41e975f58c87.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c85065ce95d86323c6c368f3c7f2b36",
+    "url": "/christmas-calendar/static/media/santassled.5c85065c.png"
+  },
+  {
+    "revision": "98806011c69a437e2a4328666ae40c10",
+    "url": "/christmas-calendar/static/media/olaf.98806011.png"
+  },
+  {
+    "revision": "f406785376f90674aee89a1942b7ed88",
+    "url": "/christmas-calendar/static/media/mikke.f4067853.gif"
+  },
+  {
+    "revision": "06eb5e330879b8b318e3a329030aaa6b",
+    "url": "/christmas-calendar/static/media/heart.06eb5e33.svg"
+  },
+  {
+    "revision": "ce47d7f7b3caeee7d27ca8029151e290",
+    "url": "/christmas-calendar/static/media/gift.ce47d7f7.svg"
+  },
+  {
+    "revision": "60437ad48bdeeb0a3c5b",
+    "url": "/christmas-calendar/static/js/runtime~main.60437ad4.js"
+  },
+  {
+    "revision": "19d7ee01e6db49e8cd55",
+    "url": "/christmas-calendar/static/js/main.19d7ee01.chunk.js"
+  },
+  {
+    "revision": "6073cd0587cc8a206cf6",
+    "url": "/christmas-calendar/static/js/1.6073cd05.chunk.js"
+  },
+  {
+    "revision": "19d7ee01e6db49e8cd55",
+    "url": "/christmas-calendar/static/css/main.8a03cc32.chunk.css"
+  },
+  {
+    "revision": "cba1a174e1d5ef05ca99e38bc8e56376",
+    "url": "/christmas-calendar/index.html"
+  }
+];
